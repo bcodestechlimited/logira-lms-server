06f4589cb1f2e31a38c2bb332540fcc2b8667468
David Bodunrin <bodunrindavidbond@gmail.com>
2026-02-20 09:47:51 +0100

    fix

--- src/utils/cloudinary.utils.ts
+++ src/utils/cloudinary.utils.ts
@@ -19,9 +19,16 @@ export const moveToSafeTemp = async (file: any) => {
     `${crypto.randomUUID()}${ext}`,
   );
 
-  await fs.mkdir(path.dirname(safePath), { recursive: true });
-  await fs.rename(file.tempFilePath, safePath);
+  // await fs.mkdir(path.dirname(safePath), { recursive: true });
+  // await fs.rename(file.tempFilePath, safePath);
 
+  await fs.mkdir(path.dirname(safePath), { recursive: true });
+
+  await fs.copyFile(file.tempFilePath, safePath);
+
+  await fs.unlink(file.tempFilePath).catch((err) => {
+    console.error("Cleanup failed, but file was moved:", err);
+  });
   return safePath;
 };
 
